2b00fde5e5f29dcce7327f1e1d0746791444169e
Catlin Millz <cat@Catlins-MacBook-Pro.local>
2024-02-09 17:07:58 -0500

    added server.js and fixed boilerplate

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,28 +1,38 @@
-const express = require('express');
-const path = require('path');
-const api = require('./routes/index.js');
+const fb = require('express').Router();
+const { readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
-const PORT = process.env.port || 3001;
-
-const app = express();
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
-app.use(express.static('public'));
-
-// GET Route
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+// GET Route for retrieving all the feedback
+fb.get('/', (req, res) =>
+  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
 );
 
-// GET Route
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+// POST Route for submitting feedback
+fb.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { email, feedbackType, feedback } = req.body;
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+  // If all the required properties are present
+  if (email && feedbackType && feedback) {
+    // Variable for the object we will save
+    const newFeedback = {
+      email,
+      feedbackType,
+      feedback,
+      feedback_id: uuid(),
+    };
+
+    readAndAppend(newFeedback, './db/feedback.json');
+
+    const response = {
+      status: 'success',
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = fb;
